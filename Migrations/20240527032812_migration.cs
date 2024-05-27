dd8e74a797403a5a5cd147b8087ce574684b5297
ElaineLi0320 <yinili0320@gmail.com>
2024-05-26 20:53:38 -0700

    initial

---
+++ Migrations/20240527032812_migration.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Routine.Api.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Companies_CompanyId",
+                table: "Employees");
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("bbdee09c-089b-4d30-bece-44df59237100"), "From Shenzhen", "Tencent" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("6fb600c1-9011-4fd7-9234-881379716400"), "From Beijing", "Baidu" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("5efc910b-2f45-43df-afae-620d40542800"), "Photoshop?", "Adobe" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("bbdee09c-089b-4d30-bece-44df59237111"), "Wow", "SpaceX" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("6fb600c1-9011-4fd7-9234-881379716411"), "Football Club", "AC Milan" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("5efc910b-2f45-43df-afae-620d40542811"), "From Jiangsu", "Suning" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("bbdee09c-089b-4d30-bece-44df59237122"), "Blocked", "Twitter" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("6fb600c1-9011-4fd7-9234-881379716422"), "Blocked", "Youtube" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("5efc910b-2f45-43df-afae-620d40542822"), "- -", "360" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("bbdee09c-089b-4d30-bece-44df59237133"), "Brothers", "Jingdong" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("6fb600c1-9011-4fd7-9234-881379716433"), "Music?", "NetEase" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("5efc910b-2f45-43df-afae-620d40542833"), "Store", "Amazon" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("bbdee09c-089b-4d30-bece-44df59237144"), "Not Exists?", "AOL" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("6fb600c1-9011-4fd7-9234-881379716444"), "Who?", "Yahoo" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Introduction", "Name" },
+                values: new object[] { new Guid("5efc910b-2f45-43df-afae-620d40542844"), "Is it a company?", "Firefox" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Companies_CompanyId",
+                table: "Employees",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Companies_CompanyId",
+                table: "Employees");
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("5efc910b-2f45-43df-afae-620d40542800"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("5efc910b-2f45-43df-afae-620d40542811"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("5efc910b-2f45-43df-afae-620d40542822"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("5efc910b-2f45-43df-afae-620d40542833"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("5efc910b-2f45-43df-afae-620d40542844"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("6fb600c1-9011-4fd7-9234-881379716400"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("6fb600c1-9011-4fd7-9234-881379716411"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("6fb600c1-9011-4fd7-9234-881379716422"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("6fb600c1-9011-4fd7-9234-881379716433"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("6fb600c1-9011-4fd7-9234-881379716444"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("bbdee09c-089b-4d30-bece-44df59237100"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("bbdee09c-089b-4d30-bece-44df59237111"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("bbdee09c-089b-4d30-bece-44df59237122"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("bbdee09c-089b-4d30-bece-44df59237133"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: new Guid("bbdee09c-089b-4d30-bece-44df59237144"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Companies_CompanyId",
+                table: "Employees",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
